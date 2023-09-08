8e3cd49c6d4c29e2abfe612bb27581813ae8254d
KhaganRustamov <142396079+KhaganRustamov@users.noreply.github.com>
2023-09-08 19:34:44 +0400

    Update search-panel.jsx

--- employees_template/src/components/search-panel/search-panel.jsx
+++ employees_template/src/components/search-panel/search-panel.jsx
@@ -2,17 +2,14 @@ import { Component } from "react";
 import "./search-panel.css";
 
 class SearchPanel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+ state = {
       term: "",
     };
-  }
-
+  
   onUpdateSearch = (e) => {
     const term = e.target.value;
     this.setState({ term });
-    this.props.onUpdateSearch(term);
+    this.onUpdateSearch(term);
   };
 
   render() {
