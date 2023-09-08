9cb55b3980b6a69263fb240def060c7deb0190ee
KhaganRustamov <142396079+KhaganRustamov@users.noreply.github.com>
2023-09-08 19:38:18 +0400

    Update employees-add-form.jsx

--- employees_template/src/components/employees-add-form/employees-add-form.jsx
+++ employees_template/src/components/employees-add-form/employees-add-form.jsx
@@ -3,13 +3,10 @@ import { Component } from "react";
 import "./employees-add-form.css";
 
 class EmployeesAddForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  state = {
       name: "",
       salary: "",
     };
-  }
 
   onValueChange = (e) => {
     this.setState({
@@ -19,7 +16,7 @@ class EmployeesAddForm extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    this.props.onAdd(this.state.name, this.state.salary);
+    this.onAdd(this.state.name, this.state.salary);
     this.setState({
       name: "",
       salary: "",
